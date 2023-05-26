c7a86845ed68639f771bec284c2e7b0878f5b8f6
Julian Bull <julianbullmagic@gmail.com>
2023-05-26 11:42:07 +1000

    ffff

--- client/src/Components/Dashboard/TestScene.js
+++ client/src/Components/Dashboard/TestScene.js
@@ -3,6 +3,13 @@ import json from './Assets/map.json'
 import mapimage from './Assets/19.07a Gentle Forest 3.0 ($5 palettes)/48x48 resize/gentle forest (48x48 resize) v05.png'
 import tree from './Assets/tree.png'
 import hero from './Assets/character.png'
+import { io } from 'socket.io-client';
+
+// "undefined" means the URL will be computed from the `window.location` object
+const URL = process.env.NODE_ENV === 'production' ?'https://onlinecommunity.onrender.com/' : 'http://localhost:3000';
+
+const socket = io(URL);
+
 console.log(json)
 export default class TestScene extends Scene {
     constructor() {
@@ -45,6 +52,45 @@ export default class TestScene extends Scene {
     this.cameras.main.startFollow(this.player,true,1,1)
     this.cameras.main.setFollowOffset(0,-this.player.height)
 
+    socket.emit('player joining', json.stringify({"name":"Jim"}))
+    function sendstate(){
+      let playerdata = {
+          "offsetx": this.player.x,
+          "offsety": this.player.y,
+          "id":this.player.id
+              }
+      socket.emit('returning state', json.stringify(playerdata))
+    }
+
+    setInterval(sendstate, 250)
+
+    socket.on('updateState',(data)=>{
+      // let tempplayers=json.parse(data)
+      // for (let player of tempplayers){
+      //   if (player['id']==app.player.id){
+      //     continue
+      //   }
+      //   if (player['id'] not in app.playernames){
+      //     this.playernames.append(player['id'])
+      //     let pos = [player['x'],player['y']]
+      //     newplayer=AnotherPlayerEntity(app, name='kitty', pos=pos, player['id'])
+      //     this.players.append(newplayer)
+      //   }
+      //   for (let play in this.players){
+      //     if (play.playername==player['id']){
+      //         play.lastpos=play.pos
+      //         play.currentpos=vec2(int(player['x']),int(player['y']))
+      //         play.pos=vec2(int(player['x']),int(player['y']))
+      //         play.incrementx=play.currentpos[0]-play.lastpos[0]
+      //         play.incrementx=int(play.incrementx/15)
+      //         play.incrementy=play.currentpos[1]-play.lastpos[1]
+      //         play.incrementy=int(play.incrementy/15)
+      //         play.positiontimer=0
+      //         play.angle=player['angle']
+      //     }
+      //   }
+      // }
+    })
   }
   update(){
     const cursors=this.input.keyboard.createCursorKeys()
